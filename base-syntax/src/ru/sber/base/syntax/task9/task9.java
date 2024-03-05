829ff552eb647cb063fd7b0f88135ed600f3086f
pc <kostacygankov@gmail.com>
2024-03-05 12:00:07 +0300

    Выполнено второе задание практики upd2

--- base-syntax/src/ru/sber/base/syntax/task9/task9.java
+++ base-syntax/src/ru/sber/base/syntax/task9/task9.java
@@ -7,30 +7,25 @@ public class task9 {
         Scanner in = new Scanner(System.in);
 
         char hideLet = (char) (Math.random() * 26 + 65);
-        //System.out.println(hideLet);
+        System.out.println(hideLet);
 
-        System.out.print("Введите букву: ");
-        char letter = in.next().toUpperCase().charAt(0);
-
+        char letter;
         boolean game = true;
         while (game) {
+            System.out.print("Введите букву: ");
+            letter = in.next().toUpperCase().charAt(0);
+
             if (letter >= 65 && letter <= 90) {
                 if (letter > hideLet) {
                     System.out.println("\nИщи букву, которая перед ней в алфавите");
-                    System.out.print("Введите букву: ");
-                    letter = in.next().toUpperCase().charAt(0);
                 } else if (letter < hideLet) {
                     System.out.println("\nИщи букву, которая после неё в алфавите");
-                    System.out.print("Введите букву: ");
-                    letter = in.next().toUpperCase().charAt(0);
                 } else {
                     System.out.println("Верно!");
                     game = false;
                 }
             } else {
                 System.out.println("\nВведён недопустимый символ");
-                System.out.print("Введите букву: ");
-                letter = in.next().toUpperCase().charAt(0);
             }
         }
     }
